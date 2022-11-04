0d27c6e75475d77eb033e423e83a6c2ebfbf6d4e
Omar Kalthoum <41536488+OmarKalthoum@users.noreply.github.com>
2022-11-04 09:44:56 +0100

    Add files via upload

---
+++ Modal/src/components/modal/Modal.js
@@ -0,0 +1,12 @@
+const Modal = (props) => {
+    return (
+        <div>
+            <div className="overlay"></div>
+            <div className="modal">
+                <h1></h1>
+                <button onClick={}> {props.btnText}</button>
+            </div>
+        </div>
+    )
+}
+export default Modal;
